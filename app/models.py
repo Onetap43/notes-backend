642903048bad00dd8782b4e31b3603b0ac7adfc1
Pranjal Singh <pranjals.singh149@gmail.com>
2026-05-14 23:56:24 +0300

    Add auth schemas and models

--- app/models.py
+++ app/models.py
@@ -2,13 +2,13 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 from app.database import DatabaseBase
 class User(DatabaseBase):
     __tablename__="users"
-    id=Column(Integer, primary_key=True, index=True)
+    user_id=Column(Integer, primary_key=True, index=True)
     username= Column(String, unique=True, index=True, nullable=False)
     password=Column(String, nullable=False)
 class Notes(DatabaseBase):
     __tablename__="notes"
     note_id=Column(Integer, primary_key=True, index=True)
-    user_id=Column(Integer, ForeignKey("users.id"), nullable=False, index=True)
+    user_id=Column(Integer, ForeignKey("users.user_id"), nullable=False, index=True)
     title= Column(String, nullable=False)
     content=Column(String, nullable=False)
 
--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,3 +1,10 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import session
+from passlib.context import CryptContext
+from jose import jwt, JWTError
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from app.database import get_database
+from app.models import User
+from app.schemas import SignupData, LoginData, TokenResponse
 router=APIRouter()
 
