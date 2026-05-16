b7c748ec316dad601157e6f6bd741964f024b027
Pranjal Singh <pranjals.singh149@gmail.com>
2026-05-16 21:52:48 +0300

    Add signup login and JWT auth

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -34,5 +34,27 @@ def get_current_user(credentials:HTTPAuthorizationCredentials=Depends(security),
     return current_user
 @router.post("/signup")
 def signup(signup_data:SignupData,database:Session=depends(get_database)):
-    
+    existing_user=database.query(User).filter(User.username==signup_data.username).first()
+    if existing_user:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="username already exists")
+    #password hash karne ka function call kiya
+    hashed_password=hash_password(signup_data.password)
+    # make a new user object and then store it in a new row in users table
+    new_user=User(username=signup_data.username,password=hashed_password)
+    database.add(new_user)
+    database.commit()
+    # now database will make automatic user_id so get that user_id in new_user
+    database.refresh(new_user)
+    return{"message":"signup successful", "user_id":new_user.user_id,
+           "username":new_user.username}
+@router.post("/login",response_model=TokenResponse)
+def login(login_data:LoginData,database:database_Session=Depends(get_database)):
+    user=database_Session.query(User).filter((User.username)==login_data.username).first()
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="invalid username or password")
+    password_is_correct=verify_password(login_data.password,user.password)
+    if  not password_is_correct:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Invalid username or password")
+    access_token=create_access_token(user.user_id)
+    return {"acess token":access_token, "token_type:"bearer"}
 
