e49404d13f35bec6e658908f3660ef0bc0b4d60e
Pranjal Singh <pranjals.singh149@gmail.com>
2026-05-18 23:41:58 +0300

    Added get all notes route

--- app/routes/notes.py
+++ app/routes/notes.py
@@ -13,3 +13,7 @@ def create_note(note_data:NoteCreate,current_user:User=Depends(get_current_user)
     #note_id was created automatically but before commit it was unknown to new_note because remember new_note was same as Note class of models and you declared all attributes like user_id and title and all but note_id is created right now so remember to refresh
     database_session.refresh(new_note)
     return{"message":"Note Created Successfully","note_id": new_note.note_id,"title":new_note.title,"content":new_note.content}
+@router.get("/notes")
+def get_my_notes(current_user:User=Depends(get_current_user),database_session:Session=Depends(get_database)):
+    my_notes=database_session.query(Notes).filter(Notes.user_id==current_user.user_id).all()
+    return my_notes
