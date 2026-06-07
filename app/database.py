b5524e590c08daa9ccd1f33af6731a68a9d32911
Pranjal Singh <pranjals.singh149@gmail.com>
2026-06-08 01:06:00 +0300

    Added connection pooling

--- app/database.py
+++ app/database.py
@@ -4,7 +4,12 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 DATABASE_URL=os.getenv("DATABASE_URL")
-database_engine=create_engine(DATABASE_URL)
+database_engine = create_engine(
+    DATABASE_URL,
+    pool_size=5,
+    max_overflow=10,
+    pool_pre_ping=True
+)
 DatabaseSession_Machine=sessionmaker(bind=database_engine, autocommit=False, autoflush=False)
 DatabaseBase = declarative_base()
 def get_database():
