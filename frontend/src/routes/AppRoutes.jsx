26747e7f8900add14e4df836104c837b95007c90
Pranjal Singh <pranjals.singh149@gmail.com>
2026-07-13 22:55:47 +0400

    Configure React Router and application routing

---
+++ frontend/src/routes/AppRoutes.jsx
@@ -0,0 +1,21 @@
+import { Routes, Route } from "react-router-dom";
+
+import Login from "../pages/Login/Login";
+import Signup from "../pages/Signup/Signup";
+import Dashboard from "../pages/Dashboard/Dashboard";
+import Profile from "../pages/Profile/Profile";
+import NotFound from "../pages/NotFound/NotFound";
+
+function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+}
+
+export default AppRoutes;
